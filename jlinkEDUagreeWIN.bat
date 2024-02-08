5547a196f9879e67721121e919b0f2d3e3beccdf
Joshelu <joshelu@gmail.com>
2024-02-08 16:07:55 +0100

    Update jlinkEDUagreeWIN.bat

--- jlinkEDUagreeWIN.bat
+++ jlinkEDUagreeWIN.bat
@@ -10,10 +10,12 @@
 set year=%Date:~6,4%
 set mon=%Date:~3,2%
 set day=%Date:~0,2%
+
 ::Use PS to convert to hex and build date hex string
 for /f %%i in ('powershell -command "'{0:x4}' -f" %year%') do set new=%%i
 for /f %%i in ('powershell -command "'{0:x2}' -f" %mon%') do set new=%new%%%i
 for /f %%i in ('powershell -command "'{0:x2}' -f" %day%') do set new=%new%%%i
+
 ::Replace old value with the new value
 reg add "HKEY_CURRENT_USER\Software\SEGGER\J-Link" /f /v "LicenseEDU_DontShowAgainToday" /t REG_DWORD /d "0x%new%"
 reg add "HKEY_CURRENT_USER\Software\SEGGER\J-Link" /f /v "LicenseEDUMini_DontShowAgainToday" /t REG_DWORD /d "0x%new%"
